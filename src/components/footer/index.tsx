1449bfba89b0dc3122db15943b0aec22194bff3c
Lucas Tassi Souza <lucastassisouza@gmail.com>
2024-12-09 21:10:03 -0300

    fix: changes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Footer = () => {
   return (
     <footer className="w-screen py-4 flex items-center justify-center text-xs">
-      Dashboard Paracord 2023. Todos os direitos reservados.
+      Dashboard Paracord 2024. Todos os direitos reservados.
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
